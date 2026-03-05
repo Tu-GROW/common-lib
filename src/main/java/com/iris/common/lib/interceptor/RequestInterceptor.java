28ecc0c36981ea06edec9e9d3acfa02c7f6e5e10
mutende <elvis.mutende@tala.co>
2026-03-06 00:03:44 +0300

    Refactor global exception handling and improve logging for better clarity

--- src/main/java/com/iris/common/lib/interceptor/RequestInterceptor.java
+++ src/main/java/com/iris/common/lib/interceptor/RequestInterceptor.java
@@ -3,7 +3,9 @@ package com.iris.common.lib.interceptor;
 import com.iris.common.lib.enums.MdcKeys;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
 import java.util.UUID;
+
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.MDC;
@@ -16,32 +18,34 @@ import org.springframework.web.servlet.HandlerInterceptor;
 @Slf4j
 public class RequestInterceptor implements HandlerInterceptor {
 
-  private static final String X_REQUEST_ID = "X-Request-Id";
+    private static final String X_REQUEST_ID = "X-Request-Id";
 
-  @Override
-  public boolean preHandle(HttpServletRequest request, @NonNull HttpServletResponse response,
-      @NonNull Object handler) {
+    @Override
+    public boolean preHandle(HttpServletRequest request, @NonNull HttpServletResponse response,
+                             @NonNull Object handler) {
 
-    String requestId = request.getHeader(X_REQUEST_ID);
-    MDC.put(MdcKeys.START_TIME.getKey(), String.valueOf(System.currentTimeMillis()));
-    if (requestId == null || requestId.isEmpty()) {
-      requestId = UUID.randomUUID().toString();
-      log.info("New incoming requestId: {} ", requestId);
-    }else{
-      log.info("New call for requestId: {}", requestId);
+        String requestId = request.getHeader(X_REQUEST_ID);
+        MDC.put(MdcKeys.START_TIME.getKey(), String.valueOf(System.currentTimeMillis()));
+        if (requestId == null || requestId.isEmpty()) {
+            requestId = UUID.randomUUID().toString();
+
+        }
+
+        log.info("New incoming requestId: {} ", requestId);
+
+        MDC.put(MdcKeys.REQUEST_ID.getKey(), requestId);
+        return true;
     }
 
-    MDC.put(MdcKeys.REQUEST_ID.getKey(), requestId);
-    return true;
-  }
+    @Override
+    public void afterCompletion(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response,
+                                @NonNull Object handler, Exception ex) {
 
-  @Override
-  public void afterCompletion(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response,
-      @NonNull Object handler, Exception ex) {
-    log.info("RequestId {} completed in {} ms", MDC.get(MdcKeys.REQUEST_ID.getKey()), MdcKeys.START_TIME.getKey());
+        long executionTime = System.currentTimeMillis() - Long.parseLong(MDC.get(MdcKeys.START_TIME.getKey()));
+        log.info("RequestId {} completed in {} ms", MDC.get(MdcKeys.REQUEST_ID.getKey()), executionTime);
 
-    MDC.remove(MdcKeys.REQUEST_ID.getKey());
-    MDC.remove(MdcKeys.START_TIME.getKey());
+        MDC.remove(MdcKeys.REQUEST_ID.getKey());
+        MDC.remove(MdcKeys.START_TIME.getKey());
 
-  }
+    }
 }
